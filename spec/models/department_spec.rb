ad097ef8b72f5582e398715921eaa395061f70e0
UberPupa <kenya777777777777@gmail.com>
2017-07-24 20:22:41 +0300

    add annotate for dep model and dep spec

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -1,20 +1,26 @@
+# == Schema Information
+#
+# Table name: departments
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Department, type: :model do
-
   describe 'name' do
-
     it 'is invalid without a name' do
       dep = build(:department, name: '')
       dep.valid?
       expect(dep.errors[:name]).to include("can't be blank")
     end
-    
     it 'is valid with name' do
       dep = build(:department)
       expect(dep.valid?).to eq true
     end
-
   end
 
 end
