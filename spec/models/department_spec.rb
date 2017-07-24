92697dc9ab68503ddb2b287afcd0b56dfc34a139
UberPupa <kenya777777777777@gmail.com>
2017-07-24 20:06:03 +0300

    add test for attribute "name" Dep model

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Department, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'name' do
+
+    it 'is invalid without a name' do
+      dep = build(:department, name: '')
+      dep.valid?
+      expect(dep.errors[:name]).to include("can't be blank")
+    end
+    
+    it 'is valid with name' do
+      dep = build(:department)
+      expect(dep.valid?).to eq true
+    end
+
+  end
+
 end
