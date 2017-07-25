9f7a331d9e41da92c58a258056ffb052ab5d4b5f
UberPupa <kenya777777777777@gmail.com>
2017-07-25 14:16:31 +0300

    Unit: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Unit.create(name: 'кг.', description: 'Килограмм')
+Unit.create(name: 'шт.', description: 'Штук')
+Unit.create(name: 'л.', description: 'Литры')
+Unit.create(name: 'порц.', description: 'Порций')
