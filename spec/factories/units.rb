e9d525dd94244da3232c1c186eb59820240f6def
UberPupa <kenya777777777777@gmail.com>
2017-07-25 13:49:29 +0300

    Unit: add annotate

--- spec/factories/units.rb
+++ spec/factories/units.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: units
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 FactoryGirl.define do
   factory :unit do
     name "MyString"
--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: units
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Unit, type: :model do
