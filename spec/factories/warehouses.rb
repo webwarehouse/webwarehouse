9bca494da63aa41eadd9971415d53c84117a9a69
UberPupa <kenya777777777777@gmail.com>
2017-07-25 11:41:30 +0300

    add WarehouseL add annotate

--- spec/factories/warehouses.rb
+++ spec/factories/warehouses.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: warehouses
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  code       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :warehouse do
     name "MyString"
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: warehouses
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  code       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
