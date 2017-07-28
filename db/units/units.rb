0e7beddd7dde5697d3f92e81c5288b331cf509b3
UberPupa <kenya777777777777@gmail.com>
2017-07-28 18:03:47 +0300

    Unit: update seeds

--- db/units/units.rb
+++ db/units/units.rb
@@ -1,4 +1,4 @@
-Unit.find_or_create_by(name: 'кг.', description: 'Килограмм')
-Unit.find_or_create_by(name: 'шт.', description: 'Штук')
-Unit.find_or_create_by(name: 'л.', description: 'Литры')
-Unit.find_or_create_by(name: 'порц.', description: 'Порций')
+Unit.find_or_create_by(name: 'кг.')   { |unit| unit.description = 'Килограмм' }
+Unit.find_or_create_by(name: 'шт.')   { |unit| unit.description = 'Штук' }
+Unit.find_or_create_by(name: 'л.')    { |unit| unit.description = 'Литры' }
+Unit.find_or_create_by(name: 'порц.') { |unit| unit.description = 'Порций' }
