98bc0f75ae9246c2ce76f76b83538de9566f0af5
volkov <volkov.slava.main@gmail.com>
2017-08-02 02:17:33 +0300

    Added Event model without tests

---
+++ app/models/event.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id           :integer          not null, primary key
+#  author       :string
+#  type         :string
+#  status       :boolean
+#  event_object :string
+#  rand_attr    :text
+#
+
+class Event < ApplicationRecord
+  validates :author, :type, :status, :event_object, :rand_attr, presence: true
+  serialize :rand_attr
+end
